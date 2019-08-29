feb28eb57fbba5d209f20c2a3329f480e8c91790
mdesai2 <54672743+mdesai2@users.noreply.github.com>
2019-08-29 08:36:27 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,11 @@
 int main(int argc, **argv)
 {
+	
+	printf("Jacob Landegren\n");
+	printf("Electrical Engineering\n");
+
 	printf("Maalav Desai\n");
-	printf("Mechanical Engineering\n")
-  printf("Jacob Landegren\n")
-  printf("Electrical Engineering\n")
+	printf("Mechanical Engineering\n");
 
   return 0;
 }
